c8aba0feb8c1c45ad4bd6b7e7c45af0160521b7c
Aashish Adhikari <140936544+anonymousboy67@users.noreply.github.com>
2025-05-06 22:04:41 +0545

    Delete images/script.js

--- images/script.js
+++
@@ -1,7 +0,0 @@
-function reveal() {
-    const msg = document.getElementById("message");
-    msg.innerText =
-      "May you become the woman you've always dreamed of — strong, kind, brilliant. The world may not know you yet, but one day it will. And I’ll always know: you were magic from the start.";
-    msg.style.display = "block";
-  }
-  
